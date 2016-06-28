7505f5bee3a7de1acf7d3b65a4645659d8ad4aa1
Anderson Araujo <ads.csharp@gmail.com>
2016-06-28 17:59:40 -0300

    Mascara de dinheiro e bugs NaN do Javascript resolvidos

--- public/static/coffee/app.coffee
+++ public/static/coffee/app.coffee
@@ -12,21 +12,52 @@ class App
 
 	iniciar_botoes = ->
 		calcular()
+		pressionar_enter()
+		mascarar_campo()
+
+	
+	mascarar_campo = ->
+		$('#valor-por-ponto').maskMoney({
+			symbol:'R$ ', 
+			showSymbol:false, 
+			thousands:'.', 
+			decimal:'.', 
+			symbolStay: false,
+			alowZero: true
+		})
+
 
 
 	pegar_valor_por_ponto = ->
 		pontos = $('#qtd-pontos').val()
+		if pontos is ""
+			$('#qtd-pontos').val(0)
+			pontos = 0 		
+			
 		valor_por_ponto = $('#valor-por-ponto').val()
-		return pontos * valor_por_ponto
+		if valor_por_ponto is "" 
+			$('#valor-por-ponto').val(0.01)
+			valor_por_ponto = 0.01
+		
+		
+		return parseFloat(pontos) * parseFloat(valor_por_ponto)
 
 
 	acrescimo_por_linha = ->
 		porc_da_linha = $('#porcentagem-da-linha').val()
+		if porc_da_linha is ""
+			$('#porcentagem-da-linha').val(0)
+			porc_da_linha = 0		
+			
 		return porc_da_linha
 
 
 	acrescimo_por_enfeites = ->
 		porcentagem_enfeites = $('#porcentagem-por-enfeites').val()
+		if porcentagem_enfeites is ""
+			$('#porcentagem-por-enfeites').val(0)
+			porcentagem_enfeites = 0
+
 		return porcentagem_enfeites
 
 
@@ -34,18 +65,21 @@ class App
 		$('#calcular').click ->			
 			total_porcentagem = 0.0
 			valor_por_ponto = pegar_valor_por_ponto()			
-			acrs_por_linha = parseFloat(acrescimo_por_linha())
-			acrs_por_enfeite = parseFloat(acrescimo_por_enfeites())
-			
-			acrs_por_linha = 0 if acrs_por_linha is ""
-			acrs_por_enfeite = 0 if acrs_por_enfeite is ""
+			acrs_por_linha = acrescimo_por_linha()
+			acrs_por_enfeite = acrescimo_por_enfeites()			
 
-			total_porcentagem += (acrs_por_linha + acrs_por_enfeite) / 100.00 * valor_por_ponto
+			total_porcentagem += (acrs_por_linha + acrs_por_enfeite) / 100.00 * valor_por_ponto			
 
 			total = valor_por_ponto + total_porcentagem
 
 			total = total.toFixed(2)			
-			$('#total').text valor_por_ponto
+			$('#total').text total
+
+	pressionar_enter = ->
+		$(document).keypress (e) ->
+			if e.which is 13
+				$('#calcular').trigger("click")
+				
 
 
 
--- public/static/js/main.js
+++ public/static/js/main.js
@@ -2,7 +2,7 @@
 var App;
 
 App = (function() {
-  var acrescimo_por_enfeites, acrescimo_por_linha, calcular, iniciar_botoes, pegar_valor_por_ponto;
+  var acrescimo_por_enfeites, acrescimo_por_linha, calcular, iniciar_botoes, mascarar_campo, pegar_valor_por_ponto, pressionar_enter;
 
   function App() {}
 
@@ -11,25 +11,54 @@ App = (function() {
   };
 
   iniciar_botoes = function() {
-    return calcular();
+    calcular();
+    pressionar_enter();
+    return mascarar_campo();
+  };
+
+  mascarar_campo = function() {
+    return $('#valor-por-ponto').maskMoney({
+      symbol: 'R$ ',
+      showSymbol: false,
+      thousands: '.',
+      decimal: '.',
+      symbolStay: false,
+      alowZero: true
+    });
   };
 
   pegar_valor_por_ponto = function() {
     var pontos, valor_por_ponto;
     pontos = $('#qtd-pontos').val();
+    if (pontos === "") {
+      $('#qtd-pontos').val(0);
+      pontos = 0;
+    }
     valor_por_ponto = $('#valor-por-ponto').val();
-    return pontos * valor_por_ponto;
+    if (valor_por_ponto === "") {
+      $('#valor-por-ponto').val(0.01);
+      valor_por_ponto = 0.01;
+    }
+    return parseFloat(pontos) * parseFloat(valor_por_ponto);
   };
 
   acrescimo_por_linha = function() {
     var porc_da_linha;
     porc_da_linha = $('#porcentagem-da-linha').val();
+    if (porc_da_linha === "") {
+      $('#porcentagem-da-linha').val(0);
+      porc_da_linha = 0;
+    }
     return porc_da_linha;
   };
 
   acrescimo_por_enfeites = function() {
     var porcentagem_enfeites;
     porcentagem_enfeites = $('#porcentagem-por-enfeites').val();
+    if (porcentagem_enfeites === "") {
+      $('#porcentagem-por-enfeites').val(0);
+      porcentagem_enfeites = 0;
+    }
     return porcentagem_enfeites;
   };
 
@@ -38,18 +67,20 @@ App = (function() {
       var acrs_por_enfeite, acrs_por_linha, total, total_porcentagem, valor_por_ponto;
       total_porcentagem = 0.0;
       valor_por_ponto = pegar_valor_por_ponto();
-      acrs_por_linha = parseFloat(acrescimo_por_linha());
-      acrs_por_enfeite = parseFloat(acrescimo_por_enfeites());
-      if (acrs_por_linha === "") {
-        acrs_por_linha = 0;
-      }
-      if (acrs_por_enfeite === "") {
-        acrs_por_enfeite = 0;
-      }
+      acrs_por_linha = acrescimo_por_linha();
+      acrs_por_enfeite = acrescimo_por_enfeites();
       total_porcentagem += (acrs_por_linha + acrs_por_enfeite) / 100.00 * valor_por_ponto;
       total = valor_por_ponto + total_porcentagem;
       total = total.toFixed(2);
-      return $('#total').text(valor_por_ponto);
+      return $('#total').text(total);
+    });
+  };
+
+  pressionar_enter = function() {
+    return $(document).keypress(function(e) {
+      if (e.which === 13) {
+        return $('#calcular').trigger("click");
+      }
     });
   };
 
